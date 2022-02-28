27de2b460697311bfbf7eaeacb225e4a78188bbe
guilhermepardo <guilherme.pardoanjos@gmail.com>
2022-02-28 20:00:03 -0300

    serie details and season details

--- src/features/series/series.controller.js
+++ src/features/series/series.controller.js
@@ -1,5 +1,23 @@
 const service = require('./series.service');
 
+exports.details = async (req, res) => {
+    try {
+        const response = await service.details(req.params.serieId, req.params.language);
+        res.send(response)
+    } catch (error) {
+        throw error;
+    }
+}
+
+exports.seasonsDetails = async (req, res) => {
+    try {
+        const response = await service.seasonsDetails(req.params.serieId, req.params.language);
+        res.send(response)
+    } catch (error) {
+        throw error;
+    }
+}
+
 exports.popular = async (req, res) => {
     try {
         const response = await service.popular(req.params.language);
--- src/features/series/series.route.js
+++ src/features/series/series.route.js
@@ -3,6 +3,8 @@ const logger = require('../../middlewares/logger.middleware')
 const router = express.Router();
 const seriesController = require('./series.controller')
 
+router.get('/details/:serieId/:language', logger.logger, seriesController.details);
+router.get('/seasons/details/:serieId/:language', logger.logger, seriesController.seasonsDetails);
 router.get('/popular/:language', logger.logger, seriesController.popular);
 
 module.exports = router;
